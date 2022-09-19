3ed3e0e1342edc67d95c30d575625ad0f908cb23
greypopsicle <liu_fenkai@163.com>
2022-09-19 18:41:10 +0800

    second commit

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     mode: 'development', // 开发模式
@@ -19,8 +20,13 @@ module.exports = {
                 use: ['style-loader', 'css-loader'] // 从右向左解析原则
             },
             {
-                test: /\.less$/,
-                use: ['style-loader', 'css-loader', 'less-loader'] // 从右向左解析原则
+                test:/\.less$/,
+                use:['style-loader','css-loader',{
+                    loader:'postcss-loader',
+                    options:{
+                        plugins:[require('autoprefixer')]
+                    }
+                },'less-loader'] // 从右向左解析原则
             },
             {
                 test: /\.js$/,
@@ -38,6 +44,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, '../public/index.html')
         }),
-        new CleanWebpackPlugin() // 打包输出前清空dist文件夹
+        new CleanWebpackPlugin(), // 打包输出前清空dist文件夹
+        new MiniCssExtractPlugin({
+            filename: "[name].[hash].css",
+            chunkFilename: "[id].css",
+        })
     ]
 }
