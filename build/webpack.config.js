0e32c43c80efc6e0a502018293f322797bb39cd1
greypopsicle <liu_fenkai@163.com>
2022-09-19 16:29:45 +0800

    first commit

---
+++ build/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+    mode: 'development', // 开发模式
+    entry: [
+        '@babel/polyfill',
+        path.resolve(__dirname, '../src/main.js')
+    ], // 入口文件
+    output: {
+        filename: '[name].[hash:8].js', // 打包后的文件名
+        path: path.resolve(__dirname, '../dist'), // 打包后的文件路径
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'] // 从右向左解析原则
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader'] // 从右向左解析原则
+            },
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    },
+                },
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, '../public/index.html')
+        }),
+        new CleanWebpackPlugin() // 打包输出前清空dist文件夹
+    ]
+}
